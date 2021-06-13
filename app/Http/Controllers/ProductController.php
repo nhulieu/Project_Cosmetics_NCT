93fb329f7b3d2fb19ac1a83d3a367f5f0a3d10c0
phatluu1996 <quynhnhu.lieu@gmail.com>
2021-06-13 23:51:17 +0700

    Initial Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\product;
+use Illuminate\Support\Collection;
+
+
+class ProductController extends Controller
+{
+    public function paginate($items, $pageLength) : Collection{
+        if(count($items)>0){
+            $pagesProduct = new Collection();
+            foreach(array_chunk($items, $pageLength) as $list_item){
+                $a = new Collection();
+                foreach($list_item as $item){
+                    $a->add(new product($item));
+                }
+                $pagesProduct->add($a);
+            }
+            return $pagesProduct;
+        }
+        return new Collection();
+    }
+
+    public function index(){
+        $products = product::all()->toArray();
+        $gridProducts = $this->paginate($products, 9);
+        //dd($gridProducts);
+        return view("index",["gridItems"=>$gridProducts, "page"=>0]);
+    }
+}
