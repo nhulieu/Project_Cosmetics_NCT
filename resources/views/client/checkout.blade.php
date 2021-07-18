9e9c8b0ace56daa3458892ecbe7843801afaca82
yifang <yifangqk@gmail.com>
2021-07-18 18:18:44 +0700

    input validation CreditCard

--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -23,23 +23,28 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <label>First Name</label>
-                                    <input readonly class="form-control" value="{{$user->fname}}" type="text" placeholder="First Name">
+                                    <input readonly class="form-control" value="{{$user->fname}}" type="text"
+                                           placeholder="First Name">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Last Name"</label>
-                                    <input readonly class="form-control" value="{{$user->lname}}" type="text" placeholder="Last Name">
+                                    <input readonly class="form-control" value="{{$user->lname}}" type="text"
+                                           placeholder="Last Name">
                                 </div>
                                 <div class="col-md-6">
                                     <label>E-mail</label>
-                                    <input readonly class="form-control" value="{{$user->email}}" type="text" placeholder="E-mail">
+                                    <input readonly class="form-control" value="{{$user->email}}" type="text"
+                                           placeholder="E-mail">
                                 </div>
                                 <div class="col-md-6">
                                     <label>Mobile No</label>
-                                    <input readonly class="form-control" value="{{$user->phone}}" type="text" placeholder="Mobile No">
+                                    <input readonly class="form-control" value="{{$user->phone}}" type="text"
+                                           placeholder="Mobile No">
                                 </div>
                                 <div class="col-md-12">
                                     <label>Address</label>
-                                    <input readonly class="form-control" value="{{$user->address}}" type="text" placeholder="Address">
+                                    <input readonly class="form-control" value="{{$user->address}}" type="text"
+                                           placeholder="Address">
                                 </div>
                             </div>
                         </div>
@@ -49,9 +54,11 @@
                     <div class="checkout-inner">
                         <div class="checkout-summary">
                             <h1>Cart Total</h1>
-                            <p class="sub-total">Sub Total<span>${{number_format(json_decode($order)->totalPrice, 2)}}</span></p>
+                            <p class="sub-total">Sub
+                                Total<span>${{number_format(json_decode($order)->totalPrice, 2)}}</span></p>
                             <p class="ship-cost">Coupon<span>-${{number_format($coupon, 2)}}</span></p>
-                            <h2>Grand Total<span>${{number_format(json_decode($order)->totalPrice-$coupon, 2)}}</span></h2>
+                            <h2>Grand Total<span>${{number_format(json_decode($order)->totalPrice-$coupon, 2)}}</span>
+                            </h2>
                         </div>
 
                         <form class="checkout-payment" onsubmit="handleGoBill(this)">
@@ -60,33 +67,35 @@
                                 <h1>Payment Methods</h1>
                                 <div class="payment-method">
                                     <div class="custom-control custom-radio">
-                                        <input type="radio"required class="custom-control-input" id="payment-1" value="0" name="paymentMethod">
+                                        <input type="radio" required class="custom-control-input" id="payment-1"
+                                               value="0" name="paymentMethod">
                                         <label class="custom-control-label" for="payment-1">Credit Card</label>
                                     </div>
                                     <div class="payment-content" id="payment-1-show">
                                         <p>
                                         <div class="col-md-12">
-                                            <label>Owner</label>
-                                            <input type="text" class="form-control" placeholder="Owner" name="owner">
+                                            <input type="text" class="form-control" placeholder="Owner" name="owner"
+                                                   style="text-transform: uppercase">
                                         </div>
                                         <div class="col-md-12">
-                                            <label>Card Number</label>
-                                            <input type="number" minlength="16" maxlength="16" class="form-control" placeholder="Card Number" name="card_number">
+                                            <input id="creditCard" class="form-control"
+                                                   placeholder="Card Number" name="card_number">
                                         </div>
                                         <div class="col-md-8">
-                                            <label>Expiration</label>
-                                            <input type="date" class="form-control" placeholder="Expiration" name="expiration">
+                                            <input id="expiration" class="form-control" placeholder="Expiration"
+                                                   name="expiration">
                                         </div>
                                         <div class="col-md-8">
-                                            <label>Security</label>
-                                            <input type="number" class="form-control" minlength="3" maxlength="3" placeholder="Security Code" name="security_code">
+                                            <input id="security_code" class="form-control" minlength="3" maxlength="3"
+                                                   placeholder="Security Code" name="security_code">
                                         </div>
                                         </p>
                                     </div>
                                 </div>
                                 <div class="payment-method">
                                     <div class="custom-control custom-radio">
-                                        <input type="radio" required class="custom-control-input" id="payment-2" value="1" name="paymentMethod">
+                                        <input type="radio" required class="custom-control-input" id="payment-2"
+                                               value="1" name="paymentMethod">
                                         <label class="custom-control-label" for="payment-2">COD</label>
                                     </div>
                                     <div class="payment-content" id="payment-2-show">
@@ -106,4 +115,25 @@
         </div>
     </div>
     <!-- Checkout End -->
+
+    <script>
+        new Cleave('#creditCard', {
+            creditCard: true,
+            onCreditCardTypeChanged: function (type) {
+                console.log(type)
+            }
+        });
+
+        new Cleave('#expiration', {
+            date: true,
+            datePattern: ['m', 'y']
+        });
+
+        new Cleave('#security_code', {
+            blocks: [3],
+            numericOnly: true
+        });
+
+
+    </script>
 @endsection
